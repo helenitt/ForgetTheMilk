bd78f00333c9babed30d7db431fb0084440c4966
Helen O'Brien <helen>
2017-02-05 22:12:15 +0000

    edits to parse due date using regular expression from input box

--- ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions; // There was another choice too
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,13 +14,28 @@ namespace ForgetTheMilk.Controllers
             return View(Tasks);
         }
 
-        public static readonly List<string> Tasks = new List<string>();
+        public static readonly List<Task> Tasks = new List<Task>();
             
         [HttpPost]
         public ActionResult Add(string task)
         {
-            Tasks.Add(task);
+            var taskItem = new Task { Description = task };
+            var dueDatePattern = new Regex(@"may\s(\d)");
+            var hasDueDate = dueDatePattern.IsMatch(task);
+            if(hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+                taskItem.DueDate = new DateTime(DateTime.Today.Year, 5, day);
+            }
+            Tasks.Add(taskItem); 
             return RedirectToAction("Index");
         }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
     }
 }
