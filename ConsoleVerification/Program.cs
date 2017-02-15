8a3ba31be6210136a3f8be3e7c66cd7b94944d67
Helen O'Brien <helen>
2017-02-15 09:37:46 +0000

    refactor ConsoleVerification to reduce duplication
    add PrintOutcome method

--- ConsoleVerification/Program.cs
+++ ConsoleVerification/Program.cs
@@ -11,40 +11,50 @@ namespace ConsoleVerification
     {
         static void Main(string[] args)
         {
+            TestDescriptionNoDueDate();
+            TestMayDueDate();
+
+            Console.ReadLine();
+        }
+
+        private static void TestDescriptionNoDueDate()
+        {
             var input = "Walk the Dog";
             Console.WriteLine("Scenario: " + input);
             var task = new Task(input);
             var descriptionShouldBe = input;
             DateTime? dueDateShouldBe = null;
-            if(descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate)
-            {
-                Console.WriteLine("SUCCESS");
-            }
-            else
-            {
-                Console.WriteLine("Description: " + task.Description + " should be " + descriptionShouldBe);
-                Console.WriteLine("Due Date: " + task.DueDate + " should be " + dueDateShouldBe);
-                Console.WriteLine("ERROR");
-            }
-            Console.WriteLine();
+            var success = descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate;
+            var failureMessage = "Description: " + task.Description + " should be " + descriptionShouldBe
+                                 + Environment.NewLine
+                                 + "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+            PrintOutcome(success, failureMessage);
+        }
 
-            input = "Holidays May 8";
+        private static void TestMayDueDate()
+        {
+            var input = "Holidays May 8";
             Console.WriteLine("Scenario: " + input);
-            task = new Task(input);
-            dueDateShouldBe = new DateTime(DateTime.Today.Year, 5, 8);
-            if(dueDateShouldBe == task.DueDate)
+            var task = new Task(input);
+            var dueDateShouldBe = new DateTime(DateTime.Today.Year, 5, 8);
+            var success = dueDateShouldBe == task.DueDate;
+            var failureMessage = "Due Date: " + task.DueDate + " should be " + dueDateShouldBe;
+
+            PrintOutcome(success, failureMessage);
+        }
+
+        private static void PrintOutcome(bool success, string failureMessage)
+        {
+            if (success)
             {
                 Console.WriteLine("SUCCESS");
             }
             else
             {
-                Console.WriteLine("Due Date: " + task.DueDate + " should be " + dueDateShouldBe);
-                Console.WriteLine("ERROR");
+                Console.WriteLine("ERROR: ");
+                Console.WriteLine(failureMessage);
             }
             Console.WriteLine();
-
-
-            Console.ReadLine();
         }
     }
 }
